ef01d8a937e5c33e5bbcc2c460c3e1e5d028d3dc
VivekChudasama <vivek.ownai@gmail.com>
2026-02-27 10:20:15 +0530

    make routes dynamic in day 7

--- Day 7/shope/controllers/admin.js
+++ Day 7/shope/controllers/admin.js
@@ -3,7 +3,10 @@ const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/add-product', {
     pageTitle: 'Add Product',
-    path: '/admin/add-product'
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
   });
 };
 
