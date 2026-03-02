a2b8c5cc2456e5788094849cf93a3a3353067afa
VivekChudasama <vivek.ownai@gmail.com>
2026-03-02 12:20:56 +0530

    [done] adding database and retrive the data form the database

---
+++ Day 8/shope/util/database.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2')
+const pool = mysql.createPool({
+    host : "localhost",
+    user : "root",
+    database : "node-complete",
+    password : "vivek@123456"
+})
+
+module.exports = pool.promise();
