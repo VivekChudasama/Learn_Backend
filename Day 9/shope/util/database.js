39adf27fb0948931bde659d9ea16073067805aec
VivekChudasama <vivek.ownai@gmail.com>
2026-03-09 15:43:14 +0530

    start using sequelize for mysql database

---
+++ Day 9/shope/util/database.js
@@ -0,0 +1,4 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('node-complete', 'root', 'vivek@123456', { dialect: 'mysql', host: 'localhost' })
+
+module.exports = sequelize
