a89a9848d413f47739f97ab10fa315b7c92d463f
VivekChudasama <vivek.ownai@gmail.com>
2026-03-31 11:47:31 +0530

    [done] add mongodb connection

--- Day 12/shope/util/database.js
+++ Day 12/shope/util/database.js
@@ -1,5 +1,15 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'vivek@123456', { dialect: 'mysql', host: 'localhost' })
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://Vivek:Vivek123456@cluster0.l8ydvc3.mongodb.net/?appName=Cluster0')
+        .then(client => {
+            console.log('connected')
+            callback(client)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect
