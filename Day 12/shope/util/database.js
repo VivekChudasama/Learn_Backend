42c65185c1254cddf86c27ea1af298adc6c84545
Vivek <vivek.ownai@gmail.com>
2026-03-31 11:52:33 +0530

    Update database.js

--- Day 12/shope/util/database.js
+++ Day 12/shope/util/database.js
@@ -2,7 +2,7 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 const mongoConnect = (callback) => {
-    MongoClient.connect('mongodb+srv://Vivek:Vivek123456@cluster0.l8ydvc3.mongodb.net/?appName=Cluster0')
+    MongoClient.connect('mongodb+srv://user:password@cluster0.l8ydvc3.mongodb.net/?appName=Cluster0')
         .then(client => {
             console.log('connected')
             callback(client)
